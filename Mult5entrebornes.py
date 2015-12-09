1a7432ed3bd6a4263bf75d95bded86a77c30dde8
JSraveen <miraa9@outlook.fr>
2015-12-09 09:35:40 +0100

    Update Mult5entrebornes.py

--- Mult5entrebornes.py
+++ Mult5entrebornes.py
@@ -1,9 +1,9 @@
-a= input("a = ")
+a= input("a = ") #entrer la borne minime#
 k=int(a)
-b= input("b = ")
+b= input("b = ")#entrer la borne max#
 z=int(b)
-valeur=0
-for x in range (k,z):
-  if x%5==0 :
-	  valeur=valeur+x
-print (valeur)
+valeur=0 #la somme finale#
+for x in range (k,z): #pour x entre les bornes#
+  if x%5==0 : #si le reste est égal à 0 alors#
+	  valeur=valeur+x #la somme prend la valeur#
+print (valeur) #affiche la valeur finale#
