fe6e74bd7984e2440cbe99eb8fb6c5358f43f442
JSraveen <miraa9@outlook.fr>
2016-01-06 09:41:44 +0100

    Create Calendrier.py

---
+++ Calendrier.py
@@ -0,0 +1,8 @@
+#Consigne : Ecrivez un script capable d'afficher la liste de tous les jours d'une année qui commence un lundi. #
+#Votre script utilisera lui-même trois listes : une des jours, une des mois et une des nombres.#
+
+print ("Voila le calendrier de l'année : ") #affiche le message#
+tableau1=["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"]
+tableau2=["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","novembre","décembre"]
+
+#En cours#
