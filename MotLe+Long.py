dd77b1466e30d64447b0bea882aefeec1f67070a
JSraveen <miraa9@outlook.fr>
2016-01-06 09:09:14 +0100

    Create MotLe+Long.py

---
+++ MotLe+Long.py
@@ -0,0 +1,13 @@
+#Consigne : Ecrivez un script qui recherche le mot le plus long dans une phrase donnée par l'utilisateur#
+
+P= input("Votre phrase : ") #L'utilisateur entre sa phrase#
+tableau=P.split()#Sépare la phrase dans un tableau /!\ on découpe avec un espace pas avec une virgule /!\#
+i=0 #on met à 0 deux variables#
+k=0
+for a in tableau: #Il éxécute le bloc pour chaque valeur du tableau#
+	i= len(a)#on appelle i la variable calculant la longueur de chaque mot#
+	if i > k:#Si la longueur du mot est supérieure à la longueur précédente, on concerve cette longueur et le mot en question#
+	  k=i
+	  V=a
+print (V)#On affiche le mot le plus long et sa valeur#
+print (k)
