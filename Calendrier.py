c9be10f8272eb3643aabb88bed07932c4cab8769
JSraveen <miraa9@outlook.fr>
2016-01-27 08:06:13 +0100

    Update Calendrier.py

--- Calendrier.py
+++ Calendrier.py
@@ -2,10 +2,19 @@
 #Votre script utilisera lui-même trois listes : une des jours, une des mois et une des nombres.#
 
 print ("Voila le calendrier de l'année : ") #affiche le message#
-tableau1=["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"]
-tableau2=["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","novembre","décembre"]
-tableau3=range(1, 366)
-for jours in tableau3:
-	print 
-	
-#En cours#
+nomjour=["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"]#tableau 1 : nom des jours#
+nommois=["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","novembre","décembre"]#tableau 2 : nom des mois#
+finmois=[31,28,30,31,30,31,31,30,31,30,31]#tableau 3 : nombre de jours dans un mois#
+numerojour=1
+indicedunomdujour=0
+for mois in nommois:
+	while numerojour<=finmois:
+		print (nomjour[indicedunomdujour])
+		print (numerojour)
+		print (mois)
+		numerojour=numerojour+1
+		if indicedunomdujour>6 :
+		    indicedunomdujour=0
+		if indicedunomdujour<=6 :
+		    indicedunomdujour=indicedunomdujour+1
+	finmois=finmois+1
