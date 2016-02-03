183f9037b8f2afddc3d358d0bfa2c125e8d53676
JSraveen <miraa9@outlook.fr>
2016-02-03 09:47:07 +0100

    Update Image.py

--- Image.py
+++ Image.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 from PIL import Image; 
-img=Image.open("C:\mozilla_firefox.png");
-print (img.size, img.format, img.mode)
-pixel=img.load();
-largeur=img.size[0];
+img=Image.open("C:\mozilla_firefox.png"); #On ouvre l'image#
+print (img.size, img.format, img.mode)#On affiche ses données (car en rgba#
+pixel=img.load();#on charge l'image dans la variable pixel#
+largeur=img.size[0];#On définit les tailles#
 hauteur=img.size[1];
-for i in range(largeur):
+for i in range(largeur):#double boucle largeur/hauteur#
 	for j in range(hauteur):
 		rouge,vert,bleu,a=pixel[i,j];
-		pixel[i,j]=(0,0,bleu);
-img.show();
+		pixel[i,j]=(0,0,bleu);#On modifie les couleurs en affichant que des tons bleus#
+img.show();#Affiche l'image#
