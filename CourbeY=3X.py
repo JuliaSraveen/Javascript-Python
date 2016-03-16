c7fb82d91554087dfc86acf39938fa579aef2334
JSraveen <miraa9@outlook.fr>
2016-03-16 09:32:55 +0100

    Update CourbeY=3X.py

--- CourbeY=3X.py
+++ CourbeY=3X.py
@@ -5,3 +5,18 @@ x = array ([0,1,2,3,4,5,6,7,8,9,10]); #tableau d'une liste, coordonnées de x et
 y = array ([0,3,6,9,12,15,18,21,24,27,30]);
 plot(x,y); 
 show() #affiche courbe#
+
+
+
+
+
+#Ou avec trinket#
+#https://trinket.io/python/
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([0,1,2,3,4,5,6,7,8,9,10])
+y = np.array([0,3,6,9,12,15,18,21,24,27,30])
+plt.plot(x, y)
+
+plt.show() # affiche la figure a l'ecran
